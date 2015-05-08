c8e2a2312f17ef16a5595d7c24a78078c6bfcd14
rafal Dzierzbinski 180157 <dzemba@op.pl>
2015-05-09 00:31:59 +0200

    test modyfikacji osoby

--- src/main/java/edu/iis/mto/integrationtest/repository/PersonRepository.java
+++ src/main/java/edu/iis/mto/integrationtest/repository/PersonRepository.java
@@ -4,7 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.iis.mto.integrationtest.model.Person;
 
+import java.util.List;
 
 	public interface PersonRepository extends JpaRepository<Person, Long> {
-
+		public List<Person> findByFirstNameLike(String firstName);
 }
--- src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
+++ src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
@@ -41,6 +41,25 @@ public class PersonRepositoryIntegrationTest extends IntegrationTest {
 		assertEquals(1, personRepository.count());
 		assertEquals(null, personRepository.findOne(1L));
 	}
+	
+		@Test
+		public void testUpdatePerson() {
+			Person person = personRepository.findOne(1L);
+			person.setLastName("Kowalski");
+			person.setFirstName("Jan");
+	
+			personRepository.save(person);
+	
+			assertEquals("Kowalski", personRepository.findOne(1L).getLastName());
+			assertEquals("Jan", personRepository.findOne(1L).getFirstName());
+			assertEquals(2, personRepository.count());
+		}
+		
+		@Test
+		public void testFindPeopleByNameLike() {
+			List<Person> people = personRepository.findByFirstNameLike("Mari%");
+			assertEquals(2, people.size());
+		}
 
 	private Person a(PersonBuilder builder) {
 		return builder.build();
