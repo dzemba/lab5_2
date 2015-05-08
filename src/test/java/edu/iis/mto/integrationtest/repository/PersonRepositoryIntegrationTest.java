ea1038743f9ada737c1ceb73929b607feaea81c9
rafal Dzierzbinski 180157 <dzemba@op.pl>
2015-05-09 00:11:11 +0200

    refaktor personrepositoryintegrationtest

--- src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
+++ src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
@@ -17,18 +17,22 @@ public class PersonRepositoryIntegrationTest extends IntegrationTest {
 
 	@Test
 	public void testCanAccessDbAndGetTestData() {
-//		List<Person> foundTestPersons = personRepository.findAll();
-//		assertEquals(2, foundTestPersons.size());
+		List<Person> foundTestPersons = personRepository.findAll();
+		assertEquals(2, foundTestPersons.size());
 	}
 
 	@Test
 	public void testSaveNewPersonAndCheckIsPersisted() {
-//		long count = personRepository.count();
-//		personRepository.save(a(person().withId(count + 1)
-//				.withFirstName("Roberto").withLastName("Mancini")));
-//		assertEquals(count + 1, personRepository.count());
-//		assertEquals("Mancini", personRepository.findOne(count + 1)
-//				.getLastName());
+		long count = personRepository.count();
+				Person person = a(person().withId(count + 1)
+						.withFirstName("Roberto").withLastName("Mancini"));
+				
+				personRepository.save(person);
+				assertEquals(count + 1, personRepository.count());
+				assertEquals("Mancini", personRepository.findOne(count + 1)
+						.getLastName());
+				
+				personRepository.delete( person );
 	}
 
 	private Person a(PersonBuilder builder) {
