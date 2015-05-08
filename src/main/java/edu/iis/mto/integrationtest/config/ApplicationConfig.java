6a3152da390f2e2b4ec0856463e290681e46191b
rafal Dzierzbinski 180157 <dzemba@op.pl>
2015-05-08 23:25:56 +0200

    uzupelnienie klasy peristanceConfig

--- src/main/java/edu/iis/mto/integrationtest/config/ApplicationConfig.java
+++ src/main/java/edu/iis/mto/integrationtest/config/ApplicationConfig.java
@@ -1,8 +1,28 @@
 package edu.iis.mto.integrationtest.config;
 
 
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ClassPathResource;
+ 
+import edu.iis.mto.integrationtest.utils.ModeUtils;
+ 
+@Configuration
+@ComponentScan("edu.iis.mto.integrationtest.repository")
+@Import(value = { PersistenceConfig.class })
 
-public class ApplicationConfig {
+ public class ApplicationConfig {
+ 
 
-	
+	@Bean
+	public static PropertyPlaceholderConfigurer getPropertyPlaceholderConfigurer() {
+		PropertyPlaceholderConfigurer resolver = new PropertyPlaceholderConfigurer();
+		resolver.setLocation(new ClassPathResource(ModeUtils.getMode()
+				.getModeName() + "-persistence.properties"));
+		return resolver;
+	}
+
 }
--- src/main/java/edu/iis/mto/integrationtest/config/PersistenceConfig.java
+++ src/main/java/edu/iis/mto/integrationtest/config/PersistenceConfig.java
@@ -1,8 +1,119 @@
-package edu.iis.mto.integrationtest.config;
+ package edu.iis.mto.integrationtest.config;
+ 
+ 
+ 
+ import java.sql.Driver;
+ 
+ import javax.sql.DataSource;
+  
+ import org.springframework.beans.factory.annotation.Value;
+ import org.springframework.context.annotation.Bean;
+ import org.springframework.context.annotation.Configuration;
+ import org.springframework.core.io.ClassPathResource;
+ import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+ import org.springframework.*;
+ import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+ import org.springframework.jdbc.datasource.init.DatabasePopulator;
+ import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+ import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+ import org.springframework.orm.jpa.JpaTransactionManager;
+ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+ import org.springframework.transaction.PlatformTransactionManager;
+ import org.springframework.transaction.annotation.EnableTransactionManagement;
+ 
 
+ 
+import edu.iis.mto.integrationtest.utils.ModeUtils;
 
+@Configuration
 
-public class PersistenceConfig {
-	
+@EnableTransactionManagement
 
-}
+@EnableJpaRepositories(basePackages = { "edu.iis.mto.integrationtest.repository" })
+
+ public class PersistenceConfig {
+ 
+	private final String SQL_SCHEMA_SCRIPT_PATH = "sql/schema-script.sql";
+	private final String SQL_FOLDER_NAME = "sql/";
+	private final String DATA_SCRIPT_FILENAME_SUFFIX = "-data-script.sql";
+
+	@Value("${database.url}")
+	private String databaseUrl;
+
+	@Value("${database.user}")
+	private String databaseUser;
+
+	@Value("${database.password}")
+	private String databasePassword;
+
+	@Value("${database.driver}")
+	private String databaseDriverClass;
+
+	@Bean(name = "dataSource")
+	public DataSource getDataSource() {
+		DataSource dataSource = createDataSource();
+		DatabasePopulatorUtils.execute(createDatabasePopulator(), dataSource);
+		return dataSource;
+	}
+
+	private DatabasePopulator createDatabasePopulator() {
+		ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+		databasePopulator.setContinueOnError(true);
+		databasePopulator.addScripts(new ClassPathResource(
+				SQL_SCHEMA_SCRIPT_PATH), new ClassPathResource(SQL_FOLDER_NAME
+				 +ModeUtils.getMode().getModeName()
+				 +DATA_SCRIPT_FILENAME_SUFFIX));
+		return databasePopulator;
+	}
+
+	private SimpleDriverDataSource createDataSource() {
+		SimpleDriverDataSource simpleDriverDataSource = new SimpleDriverDataSource();
+		Class<? extends Driver> driverClass = getDriverClass();
+		simpleDriverDataSource.setDriverClass(driverClass);
+		simpleDriverDataSource.setUrl(databaseUrl);
+		simpleDriverDataSource.setUsername(databaseUser);
+		simpleDriverDataSource.setPassword(databasePassword);
+		return simpleDriverDataSource;
+	}
+
+	@SuppressWarnings("unchecked")
+	private Class<? extends Driver> getDriverClass() {
+		try {
+			Class<?> driverClass = Class.forName(databaseDriverClass);
+			if (Driver.class.isAssignableFrom(driverClass)) {
+				return (Class<? extends Driver>) driverClass;
+			} else {
+				System.out
+						.println("database driver class is not the SQL driver ");
+				// LOGGER.error("database driver class is not the SQL driver ");
+				return null;
+			}
+		} catch (ClassNotFoundException e) {
+			System.out.println("database driver class not found" + e);
+			// LOGGER.error("database driver class not found", e);
+			return null;
+		}
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactoryBean.setDataSource(getDataSource());
+		entityManagerFactoryBean
+				.setPackagesToScan("edu.iis.mto.integrationtest.model");
+		entityManagerFactoryBean
+				.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+		return entityManagerFactoryBean;
+	}
+
+	@Bean(name = "transactionManager")
+	public PlatformTransactionManager annotationDrivenTransactionManager() {
+		return new JpaTransactionManager();
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+ }
